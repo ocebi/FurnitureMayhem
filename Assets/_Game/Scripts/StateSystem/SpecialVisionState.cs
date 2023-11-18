3eda0fe4ac3a0211d84eda7cbc2f4ba9ef65ce90
Özgür Çebi <cebiozgur@outlook.com>
2023-11-18 17:31:04 +0300

    Add beautify

--- Assets/_Game/Scripts/StateSystem/SpecialVisionState.cs
+++ Assets/_Game/Scripts/StateSystem/SpecialVisionState.cs
@@ -7,15 +7,13 @@ public class SpecialVisionState : State
     {
         base.OnStateEnter();
         Time.timeScale = 0.25f;
-        //TODO: Commented out
-        // PostProcessManager.Instance.SetSaturation(true, -1.5f);
+        PostProcessManager.Instance.SetSaturation(true, -1.5f);
     }
 
     public override void OnStateExit()
     {
         base.OnStateExit();
         Time.timeScale = 1f;
-        //TODO: Commented out
-        // PostProcessManager.Instance.SetSaturation(false);
+        PostProcessManager.Instance.SetSaturation(false);
     }
 }
